dea5993bc8c620d41248b1df603eb1afb1040b1d
gary23w <garystimpson03@gmail.com>
2020-07-10 14:24:51 -0400

    stuff

---
+++ readme.md
@@ -0,0 +1,3 @@
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/gary23w/heroku-rest-alchemy)
+
+-Heroku PostGRES SQL
---
+++ resources/stuff.py
@@ -0,0 +1,50 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.item import ItemModel
+
+class Stuff(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('STUFF',
+        type=str,
+        required=True,
+        help="Put Stuff Here"
+    )
+    parser.add_argument('STUFF',
+        type=str,
+        required=True,
+        help="Every item needs more stuff."
+    )
+
+    @jwt_required()
+    def get(self, name):
+        stuff = StuffModel.find_by_name(name)
+        if stuff:
+            return stuff.json()
+        return {'message': 'STUFF not found'}, 404
+
+    def post(self, name):
+        if StuffModel.find_by_name(name):
+            return {'message': "SOME STUFF with name '{}' already exists.".format(name)}, 400
+
+        data = Stuff.parser.parse_args()
+
+        stuff = StuffModel(name, **data)
+
+        try:
+            stuff.save_to_db()
+        except:
+            return {"message": "An error occurred inserting STUFF"}, 500
+
+        return item.json(), 201
+
+    def delete(self, name):
+        stuff = StuffModel.find_by_name(name)
+        if stuff:
+            stuff.delete_from_db()
+
+        return {'message': 'STUFF deleted'}
+
+
+class StuffList(Resource):
+    def get(self):
+        return {'stuff': [x.json() for x in StuffModel.query.all()]}
